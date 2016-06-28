479c85714313c1d57f8c95a82238cfa07110dc6d
Andrew Russo <russspruce>
2016-06-28 16:43:43 -0700

    Started with backend work of the MainActivity.java file.

--- app/src/main/java/com/epicodus/android_practice/fun_fact/MainActivity.java
+++ app/src/main/java/com/epicodus/android_practice/fun_fact/MainActivity.java
@@ -2,12 +2,20 @@ package com.epicodus.android_practice.fun_fact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Declare our view variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //
+
     }
 }
